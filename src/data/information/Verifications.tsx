eab4acb806d46e76b3b030b1aa0d60674878ac25
Samir Gupta <srgupta@bigpond.com>
2025-06-16 20:50:37 +1000

    update verifications information

--- src/data/information/Verifications.tsx
+++ src/data/information/Verifications.tsx
@@ -1,7 +1,11 @@
 import linkText from "@/lib/links/linkText";
 import replacePlaceholdersWithLinks from "@/lib/links/replacePlaceholderWithLinks";
 
-const verificationImageURL = "/information/verification-example.jpeg";
+
+
+
+
+const verificationImageURL = "/information/verification-example.png";
 
 const Verifications = () => (
     <div>
@@ -11,22 +15,22 @@ const Verifications = () => (
             accounts represent who they say they are.
         </p>
         <p>
-            The verifications are shown on any account page and look like the
-            below:
+            Verifications are shown on account pages that have them and look
+            like the below:
         </p>
         <div className="flex justify-center my-4">
             <img
                 src={verificationImageURL}
                 alt="Verification Example"
-                className="rounded-lg"
+                className="rounded-lg h-24"
             />
         </div>
         <p>
-            The verification tick '✓' that is shown after 'BUSINESS' confirms
-            that the business has successfully onboarded onto our platform,
-            ensuring it is a legitimate and operational entity. Only these
-            businesses will show up in search results and be able to send quotes
-            and receive payments. Learn more about onboarding{" "}
+            The green badge 'BUSINESS' confirms that the business has
+            successfully onboarded onto our platform, ensuring it is a
+            legitimate and operational entity. Only these businesses will show
+            up in search results and be able to send quotes and receive
+            payments. Learn more about onboarding{" "}
             {replacePlaceholdersWithLinks(
                 linkText("here.", "/docs#information-onboarding"),
             )}
@@ -40,15 +44,6 @@ const Verifications = () => (
             To receive this verification, accounts must apply (via email) with
             proof of their identity and/or business. This is a manual process
             and requires verification from our team.
-        </p>
-        <br />
-        <p>
-            The green verification is awarded based on community recognition. It
-            represents a business that consistently goes above and beyond,
-            earning high praise and trust from customers. {<br />}
-            This is a manual process and requires businesses to consistently
-            demonstrate exceptional service and reliability. It is a mark of
-            trust and excellence within the community.
         </p>
     </div>
 );
